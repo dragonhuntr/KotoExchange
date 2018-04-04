68cfa8e604dd9b2cc0242b9d32b1ca7f5afb505e
dragonhuntr <31814492+dragonhuntr@users.noreply.github.com>
2018-04-05 00:31:19 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -16,42 +16,12 @@ client.on('ready', () => {
   console.log('I am ready!')
 })
 
-client.on('messageReactionAdd', (reaction, user, message) => {
-  console.log(reaction.emoji.id)
-  if(reaction.emoji.id === '415886353063215114') {
-    toAccount = reaction.message.author.username + reaction.message.author.discriminator
-    account =  reaction.users.last().username + reaction.users.last().discriminator
-
-    emotip(reaction, 0.1);
-  } else if(reaction.emoji.id === '415886353121673216') {
-      toAccount = reaction.message.author.username + reaction.message.author.discriminator
-      account =  reaction.users.last().username + reaction.users.last().discriminator
-
-    emotip(reaction, 1);
-  } else if(reaction.emoji.id === '415886352836460547') {
-      toAccount = reaction.message.author.username + reaction.message.author.discriminator
-      account =  reaction.users.last().username + reaction.users.last().discriminator
-
-    emotip(reaction, 5);
-  } else if(reaction.emoji.id === '415886352861757441') {
-      toAccount = reaction.message.author.username + reaction.message.author.discriminator
-      account =  reaction.users.last().username + reaction.users.last().discriminator
-
-    emotip(reaction, 10);
-  } else if(reaction.emoji.id === '415886353121935360') {
-      toAccount = reaction.message.author.username + reaction.message.author.discriminator
-      account =  reaction.users.last().username + reaction.users.last().discriminator
-
-    emotip(reaction, 100);
-  }
-})
-
 client.on('message', message => {
 
   // If message has been emitted by a bot do nothing
   if (message.author.bot) return
 
-  if (message.content.startsWith('-koto')) {
+  if (message.content.startsWith('-exchange')) {
     var args = message.content.substring(1).split(' ')
     var command = args[1]
 
@@ -59,7 +29,7 @@ client.on('message', message => {
       case 'help':
         Commands.help(message)
         break
-      case 'balance':
+      case 'register':
         Commands.balance(message, Zcash)
         break
       case 'address':
